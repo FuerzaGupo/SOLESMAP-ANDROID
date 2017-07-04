a70ec4b4c81420889c141859aca722b88537ba92
Michaelsr <michaelsonccoramos@gmail.com>
2017-07-04 01:44:16 -0500

    Falta un poco mas

---
+++ app/src/main/java/com/home/example/sqllogin/Historial.java
@@ -0,0 +1,13 @@
+package com.home.example.sqllogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Historial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial);
+    }
+}
--- app/src/main/java/com/home/example/sqllogin/Principal.java
+++ app/src/main/java/com/home/example/sqllogin/Principal.java
@@ -2,9 +2,6 @@ package com.home.example.sqllogin;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -34,14 +31,14 @@ public class Principal extends AppCompatActivity
         recibir.setText(dato);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -92,13 +89,17 @@ public class Principal extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.ingresobtn) {
+            Intent ingreso = new Intent(Principal.this,Ingreso.class);
+            startActivity(ingreso);
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.salidabtn) {
+            Intent salida = new Intent(Principal.this,Salida.class);
+            startActivity(salida);
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.historialbtn) {
+            Intent historia = new Intent(Principal.this,Historial.class);
+            startActivity(historia);
 
         }
 
